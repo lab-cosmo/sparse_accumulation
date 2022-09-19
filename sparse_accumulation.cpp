73e5891a9230016d21e7a6597a39672e9caffcee
serfg <sergei.pozdnyakov@gmail.com>
2022-09-19 16:42:18 +0200

    working cpp implementation for the forward pass

--- sparse_accumulation.cpp
+++ sparse_accumulation.cpp
@@ -1,6 +1,7 @@
 #include <torch/extension.h>
 
 #include <iostream>
+using namespace torch::indexing;
 
 torch::Tensor sparse_accumulation_forward(torch::Tensor X1,
                                   torch::Tensor X2,
@@ -9,8 +10,32 @@ torch::Tensor sparse_accumulation_forward(torch::Tensor X1,
                                   torch::Tensor idx_1,
                                   torch::Tensor idx_2,
                                   torch::Tensor multipliers){
-
-    auto output = torch::zeros({X1.sizes()[0], X1.sizes()[1], output_size}, torch::kF32);
+    
+    
+    auto X1_a = X1.accessor<float, 3>();
+    auto X2_a = X2.accessor<float, 3>();
+    auto multipliers_a = multipliers.accessor<float, 1>();    
+    
+    auto output = torch::zeros({X1.sizes()[0], X1.sizes()[1], output_size}, torch::kF32);    
+    auto output_a = output.accessor<float, 3>();
+    
+    auto idx_1_a = idx_1.accessor<long, 1>();
+    auto idx_2_a = idx_2.accessor<long, 1>();
+    auto idx_output_a = idx_output.accessor<long, 1>();
+    
+    for (int index_first = 0; index_first < output.size(0); ++index_first){
+        for (int index_second = 0; index_second < output.size(1); ++index_second) {
+            for (int index = 0; index < idx_output_a.size(0); ++index) {                
+                auto first = X1_a[index_first][index_second][idx_1_a[index]];
+                auto second = X2_a[index_first][index_second][idx_2_a[index]];
+                auto third = multipliers_a[index];
+                
+                auto contribution = first * second * third;                
+                output_a[index_first][index_second][idx_output_a[index]] += contribution;
+            }
+        }
+    }
+    
     return output; 
 }
 
--- sparse_accumulation_plain_torch.py
+++ sparse_accumulation_plain_torch.py
@@ -4,7 +4,8 @@ def sparse_accumulation_loops(X1, X2, idx_output, output_size, idx_1, idx_2, mul
     device = X1.device #all tensors must be on the same device and blah, blah, blah    
     output = torch.zeros([X1.shape[0], X2.shape[1], output_size], device = device)
     for index in range(idx_output.shape[0]):
-        output[:, :, idx_output[index]] += X1[:, :, idx_1[index]] * X2[:, :, idx_2[index]] * multipliers[index] 
+        #output[:, :, idx_output[index]] += torch.mul(X1[:, :, idx_1[index]], X2[:, :, idx_2[index]]) * multipliers[index]
+        output[:, :, idx_output[index]] += X1[:, :, idx_1[index]] * X2[:, :, idx_2[index]] * multipliers[index]
     return output
                                                                        
                                                                        
