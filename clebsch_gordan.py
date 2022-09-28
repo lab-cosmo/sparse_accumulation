fd8488d2a891271075fd82e8852abedf5fb6becc
Guillaume Fraux <guillaume.fraux@epfl.ch>
2022-09-28 16:10:41 +0200

    Add benchmarking using real data

--- clebsch_gordan.py
+++ clebsch_gordan.py
@@ -1,26 +1,38 @@
-from sympy.physics.wigner import clebsch_gordan
 from sympy import S
+from sympy.physics.wigner import clebsch_gordan
+
+try:
+    import wigners
+except ImportError:
+    wigners = None
+
 import numpy as np
 
 
-def _sympy_compute(l1, l2, l, m1, m2):
-    return float(clebsch_gordan(S(l1), S(l2), S(l), S(m1), S(m2), S(m1 + m2)))
+def _compute_cg(l1, l2, l, m1, m2):
+    if wigners is None:
+        # use sympy
+        return float(clebsch_gordan(S(l1), S(l2), S(l), S(m1), S(m2), S(m1 + m2)))
+    else:
+        if abs(m1) > l1 or abs(m2) > l2 or abs(m1 + m2) > l:
+            return 0.0
+        return wigners.clebsch_gordan(l1, m1, l2, m2, l, m1 + m2)
 
 
 class ClebschGordan:
     def __init__(self, l_max):
         self.l_max_ = l_max
         self.precomputed_ = np.zeros(
-            [l_max + 1, l_max + 1, l_max + 1, 2 * l_max + 1, 2 * l_max + 1])
+            [l_max + 1, l_max + 1, l_max + 1, 2 * l_max + 1, 2 * l_max + 1]
+        )
 
         for l1 in range(l_max + 1):
             for l2 in range(l_max + 1):
                 for l in range(l_max + 1):
                     for m1 in range(-l_max, l_max + 1):
                         for m2 in range(-l_max, l_max + 1):
-                            now = _sympy_compute(l1, l2, l, m1, m2)
-                            self.precomputed_[l1, l2, l, m1 + l1,
-                                              m2 + l2] = now
+                            now = _compute_cg(l1, l2, l, m1, m2)
+                            self.precomputed_[l1, l2, l, m1 + l1, m2 + l2] = now
 
 
 def _multiply(first, second, multiplier):
@@ -31,14 +43,14 @@ def _multiply_sequence(sequence, multiplier):
     result = []
 
     for el in sequence:
-        #print(el)
-        #print(len(el))
+        # print(el)
+        # print(len(el))
         result.append([el[0], el[1], el[2] * multiplier])
     return result
 
 
 def _get_conversion(l, m):
-    if (m < 0):
+    if m < 0:
         X_re = [abs(m) + l, 1.0 / np.sqrt(2)]
         X_im = [m + l, -1.0 / np.sqrt(2)]
     if m == 0:
@@ -69,9 +81,9 @@ def _compress(sequence, epsilon=1e-15):
             for j in range(i, len(sequence)):
                 if (m1 == sequence[j][0]) and (m2 == sequence[j][1]):
                     multiplier += sequence[j][2]
-            if (np.abs(multiplier) > epsilon):
+            if np.abs(multiplier) > epsilon:
                 result.append([m1, m2, multiplier])
-    #print(len(sequence), '->', len(result))
+    # print(len(sequence), '->', len(result))
     return result
 
 
@@ -86,12 +98,11 @@ def get_real_clebsch_gordan(clebsch, l1, l2, lambd):
             X2_re, X2_im = _get_conversion(l2, m2)
 
             real_now.append(_multiply(X1_re, X2_re, clebsch[m1 + l1, m2 + l2]))
-            real_now.append(_multiply(X1_im, X2_im,
-                                      -clebsch[m1 + l1, m2 + l2]))
+            real_now.append(_multiply(X1_im, X2_im, -clebsch[m1 + l1, m2 + l2]))
 
             imag_now.append(_multiply(X1_re, X2_im, clebsch[m1 + l1, m2 + l2]))
             imag_now.append(_multiply(X1_im, X2_re, clebsch[m1 + l1, m2 + l2]))
-        #print(real_now)
+        # print(real_now)
         if (l1 + l2 - lambd) % 2 == 1:
             imag_now, real_now = real_now, _multiply_sequence(imag_now, -1)
         if mu > 0:
