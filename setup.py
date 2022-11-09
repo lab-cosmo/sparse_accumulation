d2988a08eb9c05bd94671b3404703ef743122400
Sergey Pozdnyakov <sergei.pozdnyakov@gmail.com>
2022-11-09 14:50:53 +0100

    moving all the code for cpu kernel for dedicated folder

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, Extension
 from torch.utils import cpp_extension
 
 setup(name='sparse_accumulation_active_dim_middle_cpp',
-      ext_modules=[cpp_extension.CppExtension('sparse_accumulation_active_dim_middle_cpp', ['sparse_accumulation_active_dim_middle.cpp'])],
+      ext_modules=[cpp_extension.CppExtension('sparse_accumulation_active_dim_middle_cpp', ['cpu_kernel/sparse_accumulation_active_dim_middle.cpp'])],
       cmdclass={'build_ext': cpp_extension.BuildExtension})
 
 
 setup(name='sparse_accumulation_cpp',
-      ext_modules=[cpp_extension.CppExtension('sparse_accumulation_cpp', ['sparse_accumulation.cpp'])],
+      ext_modules=[cpp_extension.CppExtension('sparse_accumulation_cpp', ['cpu_kernel/sparse_accumulation.cpp'])],
       cmdclass={'build_ext': cpp_extension.BuildExtension})
 
 setup(name='sparse_accumulation_active_dim_first_cpp',
-      ext_modules=[cpp_extension.CppExtension('sparse_accumulation_active_dim_first_cpp', ['sparse_accumulation_active_dim_first.cpp'])],
+      ext_modules=[cpp_extension.CppExtension('sparse_accumulation_active_dim_first_cpp', ['cpu_kernel/sparse_accumulation_active_dim_first.cpp'])],
       cmdclass={'build_ext': cpp_extension.BuildExtension})
