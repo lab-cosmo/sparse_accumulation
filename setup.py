885bcb7ec06ea3545287b027688ef850c3bb7e2b
Prashanth Kanduri <kanduri@cscs.ch>
2022-11-09 16:56:56 +0100

    attempt to package c++ extensions for the cpu implementations

--- setup.py
+++ setup.py
@@ -1,15 +1,45 @@
+import sys
+
 from setuptools import setup, Extension
 from torch.utils import cpp_extension
 
-setup(name='sparse_accumulation_active_dim_middle_cpp',
-      ext_modules=[cpp_extension.CppExtension('sparse_accumulation_active_dim_middle_cpp', ['cpu_kernel/sparse_accumulation_active_dim_middle.cpp'])],
-      cmdclass={'build_ext': cpp_extension.BuildExtension})
+with open("requirements.txt", "r") as f:
+    requirements = [
+        line.strip() for line in f if not line.strip().startswith("#")
+    ]
 
-
-setup(name='sparse_accumulation_active_dim_last_cpp',
-      ext_modules=[cpp_extension.CppExtension('sparse_accumulation_active_dim_last_cpp', ['cpu_kernel/sparse_accumulation_active_dim_last.cpp'])],
-      cmdclass={'build_ext': cpp_extension.BuildExtension})
+ext_first_active_dim = [
+      Extension(
+   name='sparse_accumulation_active_dim_first_cpp',
+   sources=['cpu_kernel/sparse_accumulation_active_dim_first.cpp'],
+   include_dirs=cpp_extension.include_paths(),
+   language='c++')
+]
 
 setup(name='sparse_accumulation_active_dim_first_cpp',
-      ext_modules=[cpp_extension.CppExtension('sparse_accumulation_active_dim_first_cpp', ['cpu_kernel/sparse_accumulation_active_dim_first.cpp'])],
+      ext_modules= ext_first_active_dim,
+      cmdclass={'build_ext': cpp_extension.BuildExtension})
+
+ext_middle_active_dim = [
+      Extension(
+   name='sparse_accumulation_active_dim_middle_cpp',
+   sources=['cpu_kernel/sparse_accumulation_active_dim_middle.cpp'],
+   include_dirs=cpp_extension.include_paths(),
+   language='c++')
+]
+
+setup(name='sparse_accumulation_active_dim_middle_cpp',
+      ext_modules= ext_middle_active_dim,
+      cmdclass={'build_ext': cpp_extension.BuildExtension})
+
+ext_last_active_dim = [
+      Extension(
+   name='sparse_accumulation_active_dim_last_cpp',
+   sources=['cpu_kernel/sparse_accumulation_active_dim_last.cpp'],
+   include_dirs=cpp_extension.include_paths(),
+   language='c++')
+]
+
+setup(name='sparse_accumulation_active_dim_last_cpp',
+      ext_modules= ext_middle_active_dim,
       cmdclass={'build_ext': cpp_extension.BuildExtension})
