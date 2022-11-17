5a02126ef855a67e23b78767adee58de6b16b4d4
Unknown <pkanduri@student.ethz.ch>
2022-11-17 16:50:33 +0100

    saving WIP stash

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 import sys
-
-from setuptools import setup, Extension
-from torch.utils import cpp_extension
+from setuptools import setup
+from torch.utils.cpp_extension import BuildExtension, CppExtension
 
 with open("requirements.txt", "r") as f:
     requirements = [
@@ -9,37 +8,37 @@ with open("requirements.txt", "r") as f:
     ]
 
 ext_first_active_dim = [
-      Extension(
+      CppExtension(
    name='sparse_accumulation_active_dim_first_cpp',
    sources=['cpu_kernel/sparse_accumulation_active_dim_first.cpp'],
-   include_dirs=cpp_extension.include_paths(),
+   libraries=["torch"],
    language='c++')
 ]
 
 setup(name='sparse_accumulation_active_dim_first_cpp',
       ext_modules= ext_first_active_dim,
-      cmdclass={'build_ext': cpp_extension.BuildExtension})
+      cmdclass={'build_ext': BuildExtension})
 
 ext_middle_active_dim = [
-      Extension(
+      CppExtension(
    name='sparse_accumulation_active_dim_middle_cpp',
    sources=['cpu_kernel/sparse_accumulation_active_dim_middle.cpp'],
-   include_dirs=cpp_extension.include_paths(),
+   libraries=["torch"],
    language='c++')
 ]
 
 setup(name='sparse_accumulation_active_dim_middle_cpp',
       ext_modules= ext_middle_active_dim,
-      cmdclass={'build_ext': cpp_extension.BuildExtension})
+      cmdclass={'build_ext': BuildExtension})
 
 ext_last_active_dim = [
-      Extension(
+      CppExtension(
    name='sparse_accumulation_active_dim_last_cpp',
    sources=['cpu_kernel/sparse_accumulation_active_dim_last.cpp'],
-   include_dirs=cpp_extension.include_paths(),
+   libraries=["torch"],
    language='c++')
 ]
 
 setup(name='sparse_accumulation_active_dim_last_cpp',
       ext_modules= ext_middle_active_dim,
-      cmdclass={'build_ext': cpp_extension.BuildExtension})
+      cmdclass={'build_ext': BuildExtension})
