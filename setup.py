5a9c3112072f87d5c728772ff8994eccc2ef6776
Sergey Pozdnyakov <sergei.pozdnyakov@gmail.com>
2022-12-02 15:37:57 +0100

    restructuring package organization

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, Extension, find_packages
 from torch.utils import cpp_extension
 
 ext_cuda = cpp_extension.CUDAExtension("sparse_accumulation_cuda",
-                         ["cuda_optimized/sparse_accumulation_cuda_kernel2D.cu"])
+                         ["sparse_accumulation/cuda_extension/sparse_accumulation_cuda_kernel2D.cu"])
 
-ext_first = cpp_extension.CppExtension('sparse_accumulation_active_dim_first_cpp', ['cpu_kernel/sparse_accumulation_active_dim_first.cpp'])
+ext_first = cpp_extension.CppExtension('sparse_accumulation_active_dim_first_cpp', ['sparse_accumulation/cpu_extension/sparse_accumulation_active_dim_first.cpp'])
 
-ext_middle = cpp_extension.CppExtension('sparse_accumulation_active_dim_middle_cpp', ['cpu_kernel/sparse_accumulation_active_dim_middle.cpp'])
+ext_middle = cpp_extension.CppExtension('sparse_accumulation_active_dim_middle_cpp', ['sparse_accumulation/cpu_extension/sparse_accumulation_active_dim_middle.cpp'])
 
-ext_last = cpp_extension.CppExtension('sparse_accumulation_active_dim_last_cpp', ['cpu_kernel/sparse_accumulation_active_dim_last.cpp'])
+ext_last = cpp_extension.CppExtension('sparse_accumulation_active_dim_last_cpp', ['sparse_accumulation/cpu_extension/sparse_accumulation_active_dim_last.cpp'])
 
 setup(name='sparse_accumulation',
       packages = find_packages(),
