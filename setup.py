785f937abe40b174f5be4836e1413119c56cbc43
frostedoyster <bigi.f@libero.it>
2022-12-23 06:55:27 +0100

    Allow CPU-only installation
    based on whether cuda is detected or not

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from setuptools import setup, Extension, find_packages
 from torch.utils import cpp_extension
+from torch import cuda
 
-ext_cuda = cpp_extension.CUDAExtension("sparse_accumulation_cuda",
+if cuda.is_available():
+      ext_cuda = cpp_extension.CUDAExtension("sparse_accumulation_cuda",
                          ["sparse_accumulation/cuda_extension/sparse_accumulation_cuda_kernel2D.cu"])
 
 ext_first = cpp_extension.CppExtension('sparse_accumulation_active_dim_first_cpp', ['sparse_accumulation/cpu_extension/sparse_accumulation_active_dim_first.cpp'])
@@ -10,8 +12,11 @@ ext_middle = cpp_extension.CppExtension('sparse_accumulation_active_dim_middle_c
 
 ext_last = cpp_extension.CppExtension('sparse_accumulation_active_dim_last_cpp', ['sparse_accumulation/cpu_extension/sparse_accumulation_active_dim_last.cpp'])
 
+ext_modules = [ext_first, ext_middle, ext_last]
+if cuda.is_available(): ext_modules.append(ext_cuda)
+
 setup(name='sparse_accumulation',
       packages = find_packages(),
-      ext_modules=[ext_first, ext_middle, ext_last, ext_cuda],
+      ext_modules = ext_modules,
       cmdclass={'build_ext': cpp_extension.BuildExtension})
 
